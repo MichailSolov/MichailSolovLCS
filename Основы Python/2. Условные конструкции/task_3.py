bd8a97aa28bce3c15597ed4de9951df643e1b68d
Michail <michail_solov2001@mail.ru>
2020-07-30 21:40:20 +0300

    comm

--- Основы Python/2. Условные конструкции/task_3.py
+++ Основы Python/2. Условные конструкции/task_3.py
@@ -1,3 +1,17 @@
-data = input()
+from math import sqrt
 
-print(1)
+data = input()
+task_boolean = 0
+KSqrt = sqrt(int(data.split(' ')[2]))
+
+for i in range(1, int(KSqrt) + 1):
+    if int(data.split(' ')[2]) % i == 0:
+        if (i <= int(data.split(' ')[1]) and int(data.split(' ')[2]) / i <= int(data.split(' ')[0])) or \
+                (i <= int(data.split(' ')[0]) and int(data.split(' ')[2]) / i <= int(data.split(' ')[1])):
+            print("yes")
+            task_boolean = 1
+            break
+
+
+if not task_boolean:
+    print("no")
