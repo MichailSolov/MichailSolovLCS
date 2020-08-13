76f97827995ed648ae4dfab7a2f704f2b47e9676
Michail <michail_solov2001@mail.ru>
2020-08-13 03:13:06 +0300

    comm

---
+++ Основы Python/7.Работа с разными форматами данных (json, xml)/json1.py
@@ -0,0 +1,23 @@
+import json
+from collections import Counter
+
+output = []
+str1 = ''
+
+with open('file.json', 'r', encoding="UTF-8") as JF:
+    data = json.load(JF)
+
+for i in data['rss']['channel']['items']:
+    str1 += (i['description'] + ' ')
+    str1 += (i['title'] + ' ')
+
+str2 = str1.split()
+
+str2.sort()
+
+for i in str2:
+    if len(i) >= 6:
+        output.append(i)
+
+output = Counter(output)
+print(output)
---
+++ Основы Python/7.Работа с разными форматами данных (json, xml)/xml1.py
@@ -0,0 +1,25 @@
+import xml.etree.cElementTree as cET
+from collections import Counter
+
+tree = cET.ElementTree(file='file.xml')
+root = tree.getroot()
+str1 = ''
+output = []
+
+message = root.findall('channel/item/description')
+for i in message:
+    str1 += i.text
+message = root.findall('channel/item/title')
+for i in message:
+    str1 += i.text
+
+str2 = str1.split()
+
+str2.sort()
+
+for i in str2:
+    if len(i) >= 6:
+        output.append(i)
+
+output = Counter(output)
+print(output)
